858ebfe7b84a2a1658e42ade6af65f27c2ff73d4
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-05-30 14:57:29 +0530

    commited again

--- oauth2.py
+++ oauth2.py
@@ -1,5 +1,4 @@
 from jose import JWTError, jwt
-from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from JWTToken import Verify_Token
@@ -12,7 +11,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login") # this should be the rout
 
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(token: str= Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
